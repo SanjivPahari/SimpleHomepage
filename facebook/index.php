f2786078d89f32f8b9ce2067fc8ea1c95a22b568
Sanjiv Pahari <sanjiv.pahari.20@gmail.com>
2016-02-21 10:46:17 +0545

    Added files via upload

---
+++ facebook/index.php
@@ -0,0 +1,50 @@
+<?PHP
+$pagetitle='Facebook Posts';
+require('../config.php');
+require('../header.php');
+?>
+ <div class="row">
+ 
+          <div class="col-lg-12">
+           
+            <h2 class="text-success">Facebook Posts <div class="fb-follow" data-href="https://www.facebook.com/<?PHP echo $fbid; ?>" data-layout="button_count" data-show-faces="true"></div></h2>  
+			</div>
+			</div>
+			
+<div class="row">
+          <div class="col-lg-12">
+				<div class="col-lg-6 " >
+			<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-video" data-allowfullscreen="1" data-href="https://www.facebook.com/paharisanjiv123/videos/524295787745197/"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/paharisanjiv123/videos/524295787745197/"><a href="https://www.facebook.com/paharisanjiv123/videos/524295787745197/">Lost &amp; Found</a><p>Short Funny Video :v :P</p>Posted by <a href="https://www.facebook.com/paharisanjiv123">Sanjiv Pahari</a> on Thursday, January 14, 2016</blockquote></div></div>
+			</div>
+			
+            <div class="col-lg-6 " >
+			<div id="fb-root"></div><div class="fb-post" data-href="https://www.facebook.com/paharisanjiv123/posts/523616657813110" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/paharisanjiv123/posts/523616657813110"><p>&#x915;&#x947; &#x939;&#x94b; &#x92f;&#x938;&#x94d;&#x924;&#x94b; !! School &#x91c;&#x93e;&#x926;&#x93e; &#x928;&#x93f; Class &#x939;&#x94b;&#x907;&#x928; Clan &#x938;&#x94b;&#x927;&#x91b;&#x928;&#x94d; &#x92f;&#x93e;&#x930; :P #ClashOfClans</p>Posted by <a href="https://www.facebook.com/paharisanjiv123">Sanjiv Pahari</a> on&nbsp;<a href="https://www.facebook.com/paharisanjiv123/posts/523616657813110">Tuesday, January 12, 2016</a></blockquote></div></div>
+<br>
+<div id="fb-root"></div><div class="fb-post" data-href="https://www.facebook.com/paharisanjiv123/posts/520311151476994" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/paharisanjiv123/posts/520311151476994"><p>Be Positive :)</p>Posted by <a href="https://www.facebook.com/paharisanjiv123">Sanjiv Pahari</a> on&nbsp;<a href="https://www.facebook.com/paharisanjiv123/posts/520311151476994">Sunday, January 3, 2016</a></blockquote></div></div>
+
+			</div>
+			
+	
+</div>
+
+		</div>
+		<br>
+		 <div class="row">
+ 
+          <div class="col-lg-12">
+           	<div class="col-lg-6 " >
+		<div id="fb-root"></div><div class="fb-post" data-href="https://www.facebook.com/paharisanjiv123/posts/511210679053708" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/paharisanjiv123/posts/511210679053708"><p>#JaiNepal&#x92e; &#x928;&#x947;&#x92a;&#x93e;&#x932;&#x940; &#x930; &#x92e;&#x947;&#x930;&#x94b; &#x928;&#x947;&#x92a;&#x93e;&#x932; !!&#xfeb5c;&#xfeb5c;&#xfeb5c; &#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe014;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#x1f31e;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfe041;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;&#xfeb5c;</p>Posted by <a href="https://www.facebook.com/paharisanjiv123">Sanjiv Pahari</a> on&nbsp;<a href="https://www.facebook.com/paharisanjiv123/posts/511210679053708">Thursday, December 10, 2015</a></blockquote></div></div>
+</div>
+	<div class="col-lg-6 " >
+<div id="fb-root"></div><div class="fb-post" data-href="https://www.facebook.com/paharisanjiv123/posts/475987249242718" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/paharisanjiv123/posts/475987249242718"><p>Why can&#039;t we change ?? :oCoz we r trapped in COMFORT ZONE :/It&#039;s our greed, comfort, fear, laziness that has trapped...</p>Posted by <a href="https://www.facebook.com/paharisanjiv123">Sanjiv Pahari</a> on&nbsp;<a href="https://www.facebook.com/paharisanjiv123/posts/475987249242718">Thursday, August 20, 2015</a></blockquote></div></div>
+</div>
+			</div>
+			</div>
+			
+			
+		
+<?PHP
+require('../footer.php');
+?>
+
+
