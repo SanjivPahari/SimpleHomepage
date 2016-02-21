2ca51c149ac67d8e7aabecefd04cef516a3a94d2
Sanjiv Pahari <sanjiv.pahari.20@gmail.com>
2016-02-21 10:31:07 +0545

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,24 @@
 <?PHP
-ini_set('display_errors','off'); 
+ini_set('display_errors','off'); // No Display Error
+/* Primary Info */
 $sitelink='paharisanjiv.com';
 $sitelinkhigh='PahariSanjiv.com';
+
 $personname='Sanjiv Pahari';
+
 $google_analysis='';
-$fbid='100004943416284';
+
+$fbid='100004943416284'; // Facebook ID
+
 $email='paharisanjiv123@gmail.com';
-$message='Hey! Check out my new site :)';
+
+$message='Hey! Check out my new site :)'; // Alert Message - Can be left empty also
+
 $aboutme="I'm Sanjiv Pahari, from Pokhara,Nepal , who loves working in new fields and try new things. Currently, I am reading in Class 8 in Gandaki Boarding School. ";
 $twitterid='pahari_sanjiv';
 $youtubeid='UCHB88lcRHefnnM8kOxU_oCg';
+
+// Skills & other section
 $myskills = array(
 'Lazy'=>'100',
 'Basic Web Developemt'=>'70',
@@ -23,12 +32,12 @@ $myskills = array(
 'Coming Soon..'=>'',
 
         );
-		
+		/* Main Project Section, Full List */
 				$myprojectsfull = array(
 'Simple Homepage'=>'<a href="https://github.com/sanjivpahari" target="_blank">Check on GitHub</a>'
 
         );
-		
+		/* Based on Facebook ID */
 			$myfriend = array(
 '4'=>'<b>Mark Zuckerberg</b><br>No Intro Needed! <br>CEO & Founder of Facebook ',
 '100004943416284'=>'<b>Sanjiv Pahari</b><br>I, me & myself.'
